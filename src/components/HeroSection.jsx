1a338f13f0824b52593d3ebd529ae96dffc609f2
Lucas Eifler <lucass.eifler@gmail.comm>
2025-02-07 19:41:27 +0100

    hero section aligned

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,8 @@
 const HeroSection = () => {
   return (
-    <div className="relative min-h-screen bg-secondary hero flex items-center justify-between">
+    <div className="relative min-h-screen bg-secondary hero flex items-center justify-center px-10 md:px-20">
       {/* Texto à esquerda */}
-      <div className="w-1/2 text-left">
+      <div className="w-1/2 text-left pr-10">
         <h1 className="text-4xl md:text-6xl font-bold mb-6 text-white uppercase animate__animated animate__fadeIn animate__delay-1s">
           Knowledge is what transforms our BEING
         </h1>
@@ -16,11 +16,11 @@ const HeroSection = () => {
       </div>
 
       {/* Imagem à direita */}
-      <div className="w-1/2 text-center">
+      <div className="w-1/2 text-right pl-10">
         <img
           src="/imgs/ldselogo2.png"
           alt="Logo"
-          className="max-w-full h-auto"
+          className="max-w-[80%] md:max-w-[60%] h-auto mx-auto md:mx-0"
         />
       </div>
 
