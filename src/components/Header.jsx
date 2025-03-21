60abaaf565b955f434af0d69ff2859089489d3b5
Lucas Eifler <lucass.eifler@gmail.comm>
2025-03-21 18:36:50 +0100

    added cv in hamburguer menu and contact form.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -86,14 +86,17 @@ const Header = () => {
             My Work
           </Link>*/}
           <a
-            href="#techs"
+            href="/imgs/CVLDSE.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
             className="btn btn-ghost"
             onClick={() => setIsOpen(false)}
           >
             My Resume
           </a>
           <a
-            href="#projects"
+            href="https://github.com/LDS-E/2wheelsacrosstworld"
+            target="_blank"
             className="btn btn-ghost"
             onClick={() => setIsOpen(false)}
           >
